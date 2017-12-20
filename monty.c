e5b75a9c35731d1c0e6e6f73a6189043f55d48ab
jagrvargen <jagarthewolf@gmail.com>
2017-12-20 21:13:17 +0000

    added new variable delclarations for manager struct

--- monty.c
+++ monty.c
@@ -1,9 +1,17 @@
 #include "monty.h"
 
+manager_t *manager;
+
 /**
  */
 int main(int argc, char *argv[])
 {
+	stack_t *head = NULL;
+	unsigned int line_number = 1;
+	FILE *fp = NULL;
+	char *line = NULL;
+	int numero = 0;
+
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");

c6f66494eb3c72f9cc09ca8d0c7bcb13948c35d1
jagrvargen <jagarthewolf@gmail.com>
2017-12-20 17:06:04 +0000

    removed leftover code

--- monty.c
+++ monty.c
@@ -4,22 +4,13 @@
  */
 int main(int argc, char *argv[])
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	FILE *fp;
-
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
-		exit(EXIT_FAILURE);
+	access_file(argv[1]);
 
-	while ((read = getline(&line, &len, fp)) != -1) {
-		
-	}
+	return (0);
 }

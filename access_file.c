3b4a3b6e55233535788364575fdc2c519f371f79
jagrvargen <jagarthewolf@gmail.com>
2017-12-20 17:05:44 +0000

    included line count as argument to parse line function call

--- access_file.c
+++ access_file.c
@@ -5,22 +5,24 @@
 ssize_t access_file(char *file_name)
 {
 	FILE *fp;
-	size_t len = 0, line_count = 0;
-	char *line;
+	size_t len = 0, line_count = 1;
+	char *line = NULL;
 	ssize_t read, error;
 
 	fp = fopen(file_name, "r");
         if (fp == NULL)
-                exit(EXIT_FAILURE);
-
-	while ((read = getline(&line, &len, fp)) != -1)
 	{
-		error = parse_line(&line, len);
+		printf("Error: Can't open file %s\n", *file_name);
+                exit(EXIT_FAILURE);
+	}
+	while ((read = getline(&line, &len, fp)) != EOF)
+	{
+		error = parse_line(&line, len, line_count);
 		//Pass error to an error handling function and figure out how
 		//to free line.
 		line_count++;
         }
 
-	fclose(file_name);
+	error = fclose(file_name);
 	return (read);
 }

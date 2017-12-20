844563fa287d4b50d3e2d69bdd82197f6229cfed
jagrvargen <jagarthewolf@gmail.com>
2017-12-20 19:22:47 +0000

    changed getline loop

--- access_file.c
+++ access_file.c
@@ -7,22 +7,19 @@ ssize_t access_file(char *file_name)
 	FILE *fp;
 	size_t len = 0, line_count = 1;
 	char *line = NULL;
-	ssize_t read, error;
+	ssize_t read;
 
 	fp = fopen(file_name, "r");
         if (fp == NULL)
 	{
-		printf("Error: Can't open file %s\n", *file_name);
+		printf("Error: Can't open file %s\n", file_name);
                 exit(EXIT_FAILURE);
 	}
-	while ((read = getline(&line, &len, fp)) != EOF)
+	while (read != EOF)
 	{
-		error = parse_line(&line, len, line_count);
-		//Pass error to an error handling function and figure out how
-		//to free line.
+		read = getline(&line, &len, fp))
+		parse_line(line, len, line_count, fp);
 		line_count++;
         }
-
-	error = fclose(file_name);
-	return (read);
+          //success
 }

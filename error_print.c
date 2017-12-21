ea31edf456d31fa33d1df6707fc7efbc65235fa8
jagrvargen <jagarthewolf@gmail.com>
2017-12-21 19:28:07 +0000

    added messages for invalid read and invalid instruction

--- error_print.c
+++ error_print.c
@@ -2,6 +2,16 @@
 
 void error_print(int n)
 {
+	if (n == 1)
+	{
+		printf("Error: Can't open file %s\n", manager->file_name);
+		exit(EXIT_FAILURE);
+	}
+	else if (n == 2)
+	{
+		printf("L%d: unknown instruction %s");
+		exit(EXIT_FAILURE);
+	}
 	if (n == 3)
 	{
 		printf("L%d: can't pint, stack empty\n", manager->l_n);

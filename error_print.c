46461ef3af350d6d0329d03aa2b2762d697b6f8b
jagrvargen <jagarthewolf@gmail.com>
2017-12-21 19:34:13 +0000

    moved free_manager calls to error_print function

--- error_print.c
+++ error_print.c
@@ -5,15 +5,19 @@ void error_print(int n)
 	if (n == 1)
 	{
 		printf("Error: Can't open file %s\n", manager->file_name);
+		free_manager()
 		exit(EXIT_FAILURE);
 	}
 	else if (n == 2)
 	{
 		printf("L%d: unknown instruction %s");
+		free_manager();
 		exit(EXIT_FAILURE);
 	}
-	if (n == 3)
+	if (n == 5)
 	{
 		printf("L%d: can't pint, stack empty\n", manager->l_n);
+		free_manager();
+		exit(EXIT_FAILURE);
 	}
 }

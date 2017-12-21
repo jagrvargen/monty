8c9fbac5c4dbf2d4470df3fe4c92342df7c06dfb
jagrvargen <jagarthewolf@gmail.com>
2017-12-21 20:01:53 +0000

    added error print functions

--- funcs.c
+++ funcs.c
@@ -4,23 +4,19 @@
 void push(stack_t **head, unsigned int line_number)
 {
 	stack_t *newnode;
-	printf("check push\n");
+
 	(void) head;
 	(void) line_number;
 
 	if (&(manager->h) == NULL)
-		free_manager();
-	printf("check push 2\n");
+		exit(EXIT_FAILURE);
+
 	newnode = malloc(sizeof(stack_t));
 	if (newnode == NULL)
-	{
-		printf("check push 3\n");
-		free_manager();
-		exit(EXIT_FAILURE);
-	}
+		error_print(3);
+
 	newnode->n = manager->n;
 	newnode->prev = NULL;
-	printf("manager->n is %d\n", manager->n);
 	if (manager->h == NULL)
 	{
 		newnode->next = manager->h;
@@ -40,8 +36,8 @@ void pall(stack_t **head, unsigned int line_number)
 
 	(void) head;
 	(void) line_number;
-	printf("check pall\n");
-	if (&(manager->h) == NULL)
+
+	if (&(manager->h) == NULL || manager->h == NULL)
 		return;
 	print = manager->h;
 	while (print != NULL)
@@ -54,12 +50,12 @@ void pall(stack_t **head, unsigned int line_number)
 void pint(stack_t **head, unsigned int line_number)
 {
 	stack_t *print;
-	printf("check pint\n");
+
 	(void) head;
 	(void) line_number;
 
         if (manager->h == NULL)
-		error_print(3);
+		error_print(5);
 
 	print = manager->h;
 	printf("%d\n", print->n);
@@ -72,7 +68,7 @@ void pop(stack_t **head, unsigned int line_number)
 	(void) line_number;
 
 	if (&(manager->h) == NULL)
-		error_print(4);
+		error_print(6);
 
 	temp = manager->h;
 	manager->h = manager->h->next;
@@ -88,10 +84,8 @@ void swap(stack_t **head, unsigned int line_number)
 	(void) line_number;
 
 	if (manager->h == NULL || manager->h->next == NULL)
-	{
-		free_manager();
-		error_print(5); /*Stack is less than 2 nodes long*/
-	}
+		error_print(7); /*Stack is less than 2 nodes long*/
+
 	temp_num = manager->h->n;
 	manager->h->n = manager->h->next->n;
 	manager->h->next->n = temp_num;

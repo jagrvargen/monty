bf7c383cacd39fa2839a31049fc06a98b822f9a8
jagrvargen <jagarthewolf@gmail.com>
2017-12-21 19:49:56 +0000

    added error print for no argument to push

--- parse_line.c
+++ parse_line.c
@@ -7,7 +7,6 @@ void parse_line(void)
 	unsigned int i = 0;
 	char *token = NULL;
 	char *space = " '\n''\t'";
-
 	instruction_t stack_operations[] = {
 		{"push", push},
 		{"pall", pall},
@@ -21,32 +20,26 @@ void parse_line(void)
 
 	token = strtok(manager->l, space);
 	while (token == NULL)
-	{
 		access_file();
-	}
-
 	while (stack_operations[i].opcode != NULL)
 	{
 		if (strcmp(token, stack_operations[i].opcode) == 0)
 		{
 			if (i == 0)
 			{
-				token = strtok(NULL, space);
-				if (token == NULL)
-					free_manager();
-				if (atoi(token))
+				if(token = strtok(NULL, space) && atoi(token))
 				{
 					manager->n = atoi(token);
 					stack_operations[i].f(NULL, 0);
 				}
+				else
+					error_print(4);
 			}
 			else
-			{
 				stack_operations[i].f(NULL, 0);
-			}
 		}
 		i++;
 	}
 	if (stack_operations == NULL)
-		exit(EXIT_FAILURE);
+		error_print(2);
 }

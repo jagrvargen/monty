e766d09aabe94c3115d39679d9591688b48353a4
jagrvargen <jagarthewolf@gmail.com>
2017-12-22 00:04:37 +0000

    update

--- error_print.c
+++ error_print.c
@@ -15,7 +15,7 @@ void error_print(int n)
 	else if (n == 2)
 	{
 		printf("L%d: unknown ", manager->l_n);
-		printf("insstruction %s\n", manager->file_name);
+		printf("instruction %s\n", manager->file_name);
 		free_manager();
 		exit(EXIT_FAILURE);
 	}
